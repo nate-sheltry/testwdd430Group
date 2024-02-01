b80d817d00046d1d4110a590cf3f0084e5a363cc
edgar <edgardavid1503@gmail.com>
2024-02-01 10:51:15 -0800

    corrected logo alignment

--- src/app/ui/header/logo.tsx
+++ src/app/ui/header/logo.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 export default function Logo(){
     return (
         
-        <Link href={"/"} className="ms-5" >
-            <h1 className={`${kumbhSans.className }text-very-dark-blue text-sm font-extrabold md:text-lg lg:text-xl `}>Handcrafted Haven</h1>
+        <Link href={"/"} className="ms-5 flex " >
+            <h1 className={`${kumbhSans.className }text-very-dark-blue text-sm font-extrabold items-center m-0 md:text-lg lg:text-xl `}>Handcrafted Haven</h1>
          </Link>
     )
 }
